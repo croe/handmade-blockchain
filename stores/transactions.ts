f80809d33716258c98953907ed62557c3edb4ddf
Akihiro Kato <4kihiro.kato@gmail.com>
2025-05-08 01:43:20 +0900

    feat: update tx store

--- stores/transactions.ts
+++ stores/transactions.ts
@@ -2,15 +2,9 @@ import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 import { latestTimestampUserState, currentUserState } from '@/stores/users'
 import { Transaction } from '@/models/transaction'
+import { isValidTimestamp } from '@/utils/isValidTimestamp'
 
 export const txsState = atomWithStorage<Transaction[]>('txs', [])
-//
-// export const syncedTxsState = atom<Transaction[]>((get) => {
-//   const txs = get(txsState)
-//   // 同期済みのトランザクションを取得する
-//   return txs.filter((tx) => tx.synced)
-// }
-
 
 export const syncedTxsState = atom<Transaction[]>(
   (get) => {
@@ -22,13 +16,30 @@ export const syncedTxsState = atom<Transaction[]>(
   // 降順でソート
   txs.sort((a, b) => b.timestamp - a.timestamp)
   if (latestTimestampUser) {
-    if (currentUser.timestamp > latestTimestampUser.timestamp) {
-      // 自分のタイムスタンプが最新のユーザーよりも新しい場合
+    if (isValidTimestamp(latestTimestampUser.timestamp)) {
+      /**
+       * 1. 最新の他のユーザーのタイムスタンプが現在から10秒以内の場合 ＝ 全てのトランザクションの中で最新である
+       * 最新のトランザクションを返却する
+       */
+      return txs
+    } else if (currentUser.timestamp > latestTimestampUser.timestamp) {
+      /**
+       * 2. 自分のタイムスタンプが最新のタイムスタンプよりも新しい場合 = 自分が最新である
+       * 自分のタイムスタンプよりも前のトランザクションを返却する
+       */
       return txs.filter((tx) => tx.timestamp <= currentUser.timestamp)
     } else {
+      /**
+       * 3. 最新の他のユーザーのタイムスタンプが自分のタイムスタンプよりも新しい場合 ＝ 他のユーザーの最新が最新である
+       * 他の最新ユーザーのタイムスタンプよりも前のトランザクションを返却する
+       */
       return txs.filter((tx) => tx.timestamp <= latestTimestampUser.timestamp)
     }
   } else {
+    /**
+     * 4. 最新の他のユーザーがいない場合 = 自分が最新である
+     * 自分のタイムスタンプよりも前のトランザクションを返却する
+     */
     return txs.filter((tx) => tx.timestamp <= currentUser.timestamp)
   }
 })
