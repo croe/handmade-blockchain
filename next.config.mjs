12e3469a240644c92441fc7c9301a917cfd535a8
Akihiro Kato <4kihiro.kato@gmail.com>
2025-05-20 04:16:12 +0900

    feat: update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
+import nextPWA from 'next-pwa';
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -22,4 +31,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPWA(nextConfig)
