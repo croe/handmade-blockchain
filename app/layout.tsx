562a968b1d8468c321e603331aab1a5fe368de68
Akihiro Kato <4kihiro.kato@gmail.com>
2025-05-20 04:13:08 +0900

    feat: update manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
       <title>HANDMADE BLOCKCHAIN</title>
       <link rel="manifest" href="/manifest.json" />
       <link rel="apple-touch-icon" href="/icon.png"></link>
-      <meta name="theme-color" content="#b8e986" />
     </head>
       <body id="root" suppressHydrationWarning>
         <RenderMounted>
