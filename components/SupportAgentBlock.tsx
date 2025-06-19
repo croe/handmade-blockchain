1de7f885953c82be8591c14a321595430a32a685
Akihiro Kato <4kihiro.kato@gmail.com>
2025-06-19 17:28:28 +0900

    update support agent image

--- components/SupportAgentBlock.tsx
+++ components/SupportAgentBlock.tsx
@@ -11,7 +11,7 @@ const SupportAgentBlock = ({children}: Props) => {
   return (
     <div className="flex items-start mb-8">
       <div className="min-w-[38px] mr-3">
-        <Image src="/images/icons/agent.svg" alt="サポートエージェント" width={48} height={65}/>
+        <Image src="/images/icons/support_agent.png" alt="サポートエージェント" width={48} height={65}/>
       </div>
       <div className="bg-white border border-gray-300 rounded-xl p-4 text-gray-700 text-sm shadow-sm">
         {children}
